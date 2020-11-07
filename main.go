03a861558dd02e16940aff1fc639e3192536e877
Alexander Albert <alexander@vilkrig.co.nz>
2020-11-07 09:52:26 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 Vilkrig Limited <alexander@vilkrig.com>
+Copyright © 2020 Vilkrig Europe Limited <alexander@vilkrig.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
